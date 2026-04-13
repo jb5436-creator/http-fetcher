ee4c33565f6b4b66cb03446c56ae181520ac7aff
Meowthulu <jb5436@gmail.com>
2026-04-13 14:39:42 -0700

    added redicrects, added path to generated text file

--- scanner.py
+++ scanner.py
@@ -5,15 +5,17 @@ def main():
     while True:
     #tries to connect to host
         try:
+            
             input_host = get_host()
-            status, headers, body = fetch(input_host, get_path())
+            input_path = get_path()
+            status, headers, body = fetch(input_host, input_path)
 
 
             #prints recieved data
             print(f"Status         : {status}")
             print(f"Server         : {headers.get('Server')}")     
             print(f"Body preview   : {body[:200]}")       
-            save_body(body, input_host)
+            save_body(body, input_host, input_path)
             
             break
         #if no host is found, prints error
@@ -42,7 +44,8 @@ def get_host():
             print("\nError: NO SPACES ALLOWED\n")
             continue
         return host
-        
+
+
 def get_path():
     path = input("PATH? (press enter for root): ").strip()
     if not path:
@@ -51,6 +54,7 @@ def get_path():
         path = "/" + path
     
     return path
+
 
 #grabs host and sets port to 80
 def connect(host, port=80):
@@ -61,11 +65,13 @@ def connect(host, port=80):
     plug.connect((host, port))
     return plug
 
+
 #sends request to host webpage
 def send_req(plug, host, path="/"):
     #sends binary request to host in HTTP/1.1 for webpage, ensures connection ends once data recieved
     ask = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
     plug.send(ask.encode())
+
 
 #recieves response from http connection from "plug"
 def receive_response(plug):
@@ -76,6 +82,7 @@ def receive_response(plug):
             break
         response += chunk
     return response
+
 
 #takes response and decodes it
 def parse_response(response):
@@ -96,28 +103,67 @@ def parse_response(response):
 
     return status_code, header_dict, body
 
+
 #grabs host and sets path to default to "/"
 def fetch(host, path="/"):
-    #opens connection
-    plug = connect(host)
-    #sends request to connection
-    send_req(plug, host, path)
-    #recieves binary response
-    response = receive_response(plug)
-    #closes connection
-    plug.close()
+    MAX_REDIRECTS = 5
+    redirects = 0
 
-    #unpacks the tuple returned by parse_response into three separate variables
-    status, headers, body = parse_response(response)
+    while redirects < MAX_REDIRECTS:
+        #opens connection
+        plug = connect(host)
+        #sends request to connection
+        send_req(plug, host, path)
+        #recieves binary response
+        response = receive_response(plug)
+        #closes connection
+        plug.close()
+
+        #unpacks the tuple returned by parse_response into three separate variables
+        status, headers, body = parse_response(response)
+
+        #checks status of recieved decoded binary file, if a redirect it passes to to location
+        if status in ("301", "302", "303", "307", "308"):
+            #grabs location name from headers dict
+            location = headers.get("Location")
+            #if theres no location it breaks
+            if not location:
+                break
+
+            print(f"REDIRECTING TO {location}")
+            #removes prefix from http or https url
+            location = location.replace("https://", "").replace("http://", "")
+            if "/" in location:
+                #on the first / in location, it splits it adds the second index(path) to the grander path variable
+                host, path = location.split("/", 1)
+                path = "/" + path
+                
+            else:
+                #otherwise if theres no / host becomes current location
+                host = location
+                #and path becomes root
+                path = "/"
+            
+            #adds 1 to the redirect tally
+            redirects += 1
+        
+        #if not a redirect status breaks the loop
+        else:
+            break
+    
+    if redirects == MAX_REDIRECTS:
+        print("MAXIMUM REDIRECTS")
+    
     return status, headers, body
 
-def save_body(body, host):
+
+def save_body(body, host, input_path):
     folder = "fetched"
     #creates/looks for fetched folder. exists_ok=True prevents crash if folder already exists
     os.makedirs(folder, exist_ok=True)
 
     #makes name savable by replacing . and / with _
-    filename = host.replace(".", "_").replace("/", "_") + ".txt"
+    filename = host.replace(".", "_") + input_path.replace("/", "_") + ".txt"
     #adds filename to folder
     filepath = os.path.join(folder, filename)
 
